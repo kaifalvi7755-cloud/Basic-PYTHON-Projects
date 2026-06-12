656f3e84b2275690bd1b7284f7b669fe173dbecc
Kaif Alvi <kaifalvi7755@gmail.com>
2026-06-13 03:40:45 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a=int(input("enter 1st number"))
+b=int(input("input 2nd number"))
+c=input("give operator(+,-,*,/,avg)")
+if c== "+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="*":
+    print(a*b)
+elif c=="/":
+    print(a/b)
+elif c=="avg":
+    print((a+b)/2)
+else :
+    print("it s invald.\nvalid is ➡️+,-,*,/,avg")
